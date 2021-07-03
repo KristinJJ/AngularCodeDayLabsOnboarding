ae4ee7c8ccb7fc7592b20fd3ce0bc22f51c4e7a0
KristinJJ <kristin.jue@gmail.com>
2021-07-03 14:39:53 -0700

    Added shipping prices link to cart view

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-shipping',
@@ -6,10 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shipping.component.css']
 })
 export class ShippingComponent implements OnInit {
+  shippingCosts = this.cartService.getShippingPrices();
+  constructor(private cartService: CartService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
