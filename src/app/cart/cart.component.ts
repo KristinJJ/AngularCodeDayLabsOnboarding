78aa6d1d97e1d056d02ade382c297eb30fa368bb
KristinJJ <kristin.jue@gmail.com>
2021-07-03 14:27:33 -0700

    Added functionality to display cart items

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  constructor() {}
+  items = this.cartService.getItems();
+
+  constructor(private cartService: CartService) {}
 
   ngOnInit() {}
 }
