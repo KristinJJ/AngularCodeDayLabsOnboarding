027560899c689e43c68944ed8aecd64678271b03
KristinJJ <kristin.jue@gmail.com>
2021-07-03 14:32:25 -0700

    Added HTTP client to CartService constructor

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   items: Product[] = [];
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   addToCart(product: Product) {
     this.items.push(product);
